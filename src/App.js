cdc489fd4d1bc6e34333370125e3501d6bf8e0b2
t.tabuchi <t.tabuchi@gemcook.com>
2020-02-02 20:09:24 +0900

    create class componet and functional componet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-    <div>
-      <label htmlFor="bar">bar</label>
-       <input type="text" onChange={() => {console.log("I am clicked")}}/>
-    </div>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//        <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//     </div>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+  </div>
+  )
 }
+
+const Cat = () => {
+  return <div>meow</div>
+}
+
+
 
 export default App;
